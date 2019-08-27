e1e8e2c4ae6658bf753e6fc95d2d68beb17f799a
Dylan Corvidae <dylancorvidae@gmail.com>
2019-08-27 11:20:05 -0700

    added fil structure, built html and css up

---
+++ public/server.js
@@ -0,0 +1,34 @@
+// Load Environment Variables from the .env file
+require('dotenv').config();
+
+// Application Dependencies
+const express = require('express');
+const cors = require('cors');
+const morgan = require('morgan');
+const client = require('./lib/client');
+
+// Database Client
+client.connect();
+
+// Application Setup
+const app = express();
+const PORT = process.env.PORT;
+app.use(morgan('dev')); // http logging
+app.use(cors()); // enable CORS request
+app.use(express.static('public')); // enable serving files from public
+app.use(express.json()); // enable reading incoming json data
+
+
+
+
+
+
+
+
+
+
+
+// Start the server
+app.listen(PORT, () => {
+    console.log('server running on PORT', PORT);
+});
