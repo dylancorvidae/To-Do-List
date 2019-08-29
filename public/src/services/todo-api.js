31eab2d79410f642607bf71939074e41e4de4501
Dylan Corvidae <dylancorvidae@gmail.com>
2019-08-29 11:29:35 -0700

    added most of the auth stuff, still need to create tables and get login working

--- public/src/services/todo-api.js
+++ public/src/services/todo-api.js
@@ -1,8 +1,23 @@
+import store from './store.js';
+
 const URL = '/api';
 
+const token = store.getToken();
+if(!token && location.pathname !== '/auth.html') {
+    const searchParams = new URLSearchParams();
+    searchParams.set('redirect', location.pathname);
+    location = `auth.html?${searchParams.toString()}`;
+}
+
 function fetchWithError(url, options) {
+    if(token) {
+        options = options || {};
+        options.headers = options.headers || {};
+        options.headers.Authorization = token;
+    }
+
     return fetch(url, options)
-        .then(response => {            
+        .then(response => {
             if(response.ok) {
                 return response.json();
             }
@@ -49,3 +64,24 @@ export function removeTask(id) {
     });
 }
 
+export function signUp(user) {
+    const url = `${URL}/auth/signup`;
+    return fetchWithError(url, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(user)        
+    });
+}
+
+export function signIn(credentials) {
+    const url = `${URL}/auth/signin`;
+    return fetchWithError(url, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(credentials)        
+    });
+}
