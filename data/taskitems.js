6a38255888eb25ed885f063a2144bebb2d3df8ff
Dylan Corvidae <dylancorvidae@gmail.com>
2019-08-28 17:33:16 -0700

    final formatting update

--- data/taskitems.js
+++ data/taskitems.js
@@ -1,17 +1,22 @@
 module.exports = [
     {
-        name: 'Be an inspirational and present parent', completed: false
+        name: 'Be an inspirational and present parent', 
+        completed: false
     },
     {
-        name: 'Practice Code', completed: false
+        name: 'Practice Code', 
+        completed: false
     },
     {
-        name: 'Practice Trapeze', completed: false
+        name: 'Practice Trapeze', 
+        completed: false
     },
     {
-        name: 'Practice guitar', completed: false
+        name: 'Practice guitar', 
+        completed: false
     },
     {
-        name: 'Practice piano', completed: false
+        name: 'Practice piano', 
+        completed: false
     }
 ];
